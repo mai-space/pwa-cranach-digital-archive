3f5973515f7ed77bc7d12b95489f2268a7a592a1
Moritz D <52575034+inf155@users.noreply.github.com>
2019-09-03 15:45:23 +0200

    Info Page Structure + Image View
    To-Do: Diverse Styling

--- CDAA/assets/js/navigation.js
+++ CDAA/assets/js/navigation.js
@@ -1,13 +1,15 @@
 function changePage(page) {
+    document.getElementById('infoImageView').style.display = 'none';
+
     if (page != 'scan') {
         stopScanner();
         document.getElementById('scan').style.display = 'none';
         document.body.style.overflow = 'auto';
-        document.getElementById('scanicon').style.filter = 'invert(36%) sepia(80%) saturate(5758%) hue-rotate(230deg) brightness(70%) contrast(90%)';
     } else {
         startScanner();
         document.getElementById('scan').style.display = 'block';
         document.body.style.overflow = 'hidden';
+		document.getElementById('scanicon').style.filter = 'invert(36%) sepia(80%) saturate(5758%) hue-rotate(230deg) brightness(70%) contrast(90%)';
     }
 
     if (page != 'info') {
@@ -43,6 +45,23 @@ $(document).ready(function(){
 
     $(document).on('click','.recentRow', function() {
         updateInfo($(this).data("gallery-id"));
+        changePage('info');
+    });
+
+    $(document).on('click','.detailImages', function() {
+        if (window.navigator.onLine) {
+            updateImageView($(this).data("gallery-id")).then(() => {
+                document.getElementById('infoContent').style.display = 'none';
+                document.getElementById('infoImageView').style.display = 'block';
+            });
+        } else {
+            alert('Error: Internet Connection Required!');
+        }
+    });
+
+    $(document).on('click','.goBack', function() {
+        updateInfo($(this).data("gallery-id"));
+        document.getElementById('infoContent').style.display = 'block';
         changePage('info');
     });
 });
